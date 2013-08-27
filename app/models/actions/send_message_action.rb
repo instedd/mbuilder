3a06273f920427feb343737223bff2ffee2c008b
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-08-27 14:49:35 -0300

    Correctly persist "send message" recipient

--- app/models/actions/send_message_action.rb
+++ app/models/actions/send_message_action.rb
@@ -8,7 +8,7 @@ class Actions::SendMessageAction < Action
   end
 
   def self.from_hash(hash)
-    new(MessageBinding.from_list(hash['message']), hash['recipient'])
+    new(MessageBinding.from_list(hash['message']), MessageBinding.from_hash(hash['recipient']))
   end
 
   def as_json
