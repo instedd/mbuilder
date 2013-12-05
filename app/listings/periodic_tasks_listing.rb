5f903a60a1410ce73eba7bff5865496b8967221e
Mariano Abel Coca <marianoabelcoca@gmail.com>
2013-12-05 17:59:20 -0300

    Fixed a bug that occured when the periodic task's las run was nil.

--- app/listings/periodic_tasks_listing.rb
+++ app/listings/periodic_tasks_listing.rb
@@ -12,11 +12,11 @@ class PeriodicTasksListing < Listings::Base
   # Next run at 12:34 (in 3 minutes), last run on Wed 13 12:23 (1 day ago)
 
   column 'Rext run' do |trigger|
-    distance_of_time_in_words(Time.now, trigger.schedule.next_occurrence(Time.now).to_time, true)
+    distance_of_time_in_words(Time.now, trigger.schedule.next_occurrence(Time.now).to_time, true) if trigger.schedule.next_occurrence(Time.now)
   end
 
   column 'Last run' do |trigger|
-    distance_of_time_in_words(Time.now, trigger.schedule.previous_occurrence(Time.now).to_time, true) + ' ago'
+    distance_of_time_in_words(Time.now, trigger.schedule.previous_occurrence(Time.now).to_time, true) + ' ago' if trigger.schedule.previous_occurrence(Time.now)
   end
 
   column '', class: 'right' do |trigger|
