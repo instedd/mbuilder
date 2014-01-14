e6046dd7d15057cea81190d56d0576e2114ae09e
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-01-14 11:06:21 -0300

    Added another foreach spec

--- spec/models/foreach_spec.rb
+++ spec/models/foreach_spec.rb
@@ -25,4 +25,33 @@ describe "Foreach" do
       {"phone_copy" => 1234.0, "name_copy" => "John"},
       {"phone_copy" => 1234.0, "name_copy" => "Peter"}
   end
+
+  it "loops and selects entities" do
+    add_data "users", [
+      {"phone" => 1234.0, "name" => "Foo"},
+      {"phone" => 1234.0, "name" => "Bar"},
+      {"phone" => 5678.0, "name" => "Baz"},
+    ]
+
+    add_data "copied", [
+      {"phone_copy" => 2345.0, "name_copy" => "Foo"},
+      {"phone_copy" => 3456.0, "name_copy" => "Bar"},
+    ]
+
+    new_trigger do
+      message "filter {Phone}"
+      select_entity "users.phone = {phone}"
+      foreach("users") do
+        select_entity "copied.name_copy = *name"
+        send_message "*phone_copy", "{*name_copy}"
+      end
+    end
+
+    ctx = accept_message 'sms://1234', 'filter 1234'
+
+    assert_sets_equal ctx.messages, [
+      {from: "app://mbuilder", to: "sms://2345", body: "Foo", :"mbuilder-application" => application.id},
+      {from: "app://mbuilder", to: "sms://3456", body: "Bar", :"mbuilder-application" => application.id},
+    ]
+  end
 end
