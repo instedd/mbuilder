1d5e1621de0ba312f2432d94a848cce97f69cb53
Brian J. Cardiff <bcardiff@manas.com.ar>
2014-09-10 15:07:21 -0300

    setup RestClient logger in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,6 @@ Mbuilder::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { host: 'mbuilder-stg.instedd.org' }
+
+  RestClient.log = 'stdout'
 end
