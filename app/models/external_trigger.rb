9235778336f3b33b7b54ecce3bca5d704a45d74e
Mariano Abel Coca <marianoabelcoca@gmail.com>
2014-10-28 15:58:37 -0300

    Add named scopes for auth_method

--- app/models/external_trigger.rb
+++ app/models/external_trigger.rb
@@ -5,7 +5,7 @@ class ExternalTrigger < Trigger
   serialize :parameters
   serialize :actions
 
-  symbolize :auth_method, :in => [:basic_auth, :auth_token, :oauth], :scopes => true, :default => :basic_auth
+  symbolize :auth_method, :in => [:basic_auth, :auth_token, :oauth], :scopes => true, :default => :basic_auth, :scopes => :shallow
 
   # def route
   #   name + (parameters.map {|parameter| "#{parameter.name}=#{parameter.name}" }.join '&')
