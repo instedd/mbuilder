7db0ba96a0976bd8a4db4c66706f176ed40ee706
Adrian Romero <aromero@manas.com.ar>
2015-10-09 14:18:06 -0300

    Fixed telemetry settings

--- config/initializers/instedd_telemetry.rb
+++ config/initializers/instedd_telemetry.rb
@@ -1,11 +1,14 @@
 InsteddTelemetry.setup do |config|
 
   # Load settings from yml
-  custom_config = Rails.configuration.telemetry_configuration rescue {}
+  config_path = File.join(Rails.root, 'config', 'telemetry.yml')
+  custom_config = File.exists?(config_path) ? YAML.load_file(config_path).with_indifferent_access : nil
 
-  conf.server_url           = custom_config[:server_url]                   if custom_config.include? :server_url
-  conf.period_size          = custom_config[:period_size_days].days        if custom_config.include? :period_size_days
-  conf.process_run_interval = custom_config[:run_interval_minutes].minutes if custom_config.include? :run_interval_minutes
+  if custom_config.present?
+    config.server_url           = custom_config[:server_url]                   if custom_config.include? :server_url
+    config.period_size          = custom_config[:period_size_days].days        if custom_config.include? :period_size_days
+    config.process_run_interval = custom_config[:run_interval_minutes].minutes if custom_config.include? :run_interval_minutes
+  end
 
   # Add custom collectors to Telemetry
   config.add_collector Telemetry::ApplicationCountCollector
