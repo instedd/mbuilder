b36abc53bac758b93c4e325b5d2a634adc0888ee
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-08-03 17:13:41 -0300

    Take version for ENV

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -3,5 +3,5 @@ VersionFilePath = "#{::Rails.root.to_s}/VERSION"
 Mbuilder::Application.config.send "version_name=", if FileTest.exists?(VersionFilePath) then
   IO.read(VersionFilePath)
 else
-  "development"
+  ENV["MBUILDER_VERSION"] || "development"
 end
